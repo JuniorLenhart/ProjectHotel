f5d9c02facaa7a17013c18167c46113ae9736e1b
George <George@DESKTOP-VQBB2T8>
2018-09-12 00:37:08 -0300

    #33 - Corrigido tela de consumiveis.

--- Hotel/src/hotel/controller/ConsumivelController.java
+++ Hotel/src/hotel/controller/ConsumivelController.java
@@ -47,12 +47,16 @@ public class ConsumivelController extends BaseController<Consumivel> {
                     lTabela = new Object[listConsumivel.size()][6];
                     for (Consumivel c : listConsumivel) {
                         String situacao = (c.getIndSituacao().equals("A") ? "Ativo" : "Excluído");
+                        String tipo = (c.getTipConsumivel().equals("C") ? "Café da manhã"
+                                : (c.getTipConsumivel().equals("A") ? "Almoço"
+                                : (c.getTipConsumivel().equals("J") ? "Janta"
+                                : (c.getTipConsumivel().equals("L") ? "Lanche" : "Bebida"))));
 
                         lTabela[lLinha][0] = c.getCodConsumivel();
                         lTabela[lLinha][1] = c.getNomConsumivel();
                         lTabela[lLinha][2] = c.getDesConsumivel();
                         lTabela[lLinha][3] = c.getVlrConsumivel();
-                        lTabela[lLinha][4] = c.getTipConsumivel();
+                        lTabela[lLinha][4] = tipo;
                         lTabela[lLinha][5] = situacao;
                         lLinha++;
                     }
@@ -63,12 +67,16 @@ public class ConsumivelController extends BaseController<Consumivel> {
                     lTabela = new Object[listConsumivel.size()][6];
                     for (Consumivel c : listConsumivel) {
                         String situacao = (c.getIndSituacao().equals("A") ? "Ativo" : "Excluído");
-
+                        String tipo = (c.getTipConsumivel().equals("C") ? "Café da manhã"
+                                : (c.getTipConsumivel().equals("A") ? "Almoço"
+                                : (c.getTipConsumivel().equals("J") ? "Janta"
+                                : (c.getTipConsumivel().equals("L") ? "Lanche" : "Bebida"))));
+                        
                         lTabela[lLinha][0] = c.getCodConsumivel();
                         lTabela[lLinha][1] = c.getNomConsumivel();
                         lTabela[lLinha][2] = c.getDesConsumivel();
                         lTabela[lLinha][3] = c.getVlrConsumivel();
-                        lTabela[lLinha][4] = c.getTipConsumivel();
+                        lTabela[lLinha][4] = tipo;
                         lTabela[lLinha][5] = situacao;
                         lLinha++;
                     }
@@ -80,13 +88,17 @@ public class ConsumivelController extends BaseController<Consumivel> {
                         JOptionPane.showMessageDialog(null, "Consumível não encontrado pelo cóodigo: " + pParam);
                     } else {
                         String situacao = (consumivel.getIndSituacao().equals("A") ? "Ativo" : "Excluído");
+                        String tipo = (consumivel.getTipConsumivel().equals("C") ? "Café da manhã"
+                                : (consumivel.getTipConsumivel().equals("A") ? "Almoço"
+                                : (consumivel.getTipConsumivel().equals("J") ? "Janta"
+                                : (consumivel.getTipConsumivel().equals("L") ? "Lanche" : "Bebida"))));
 
                         lTabela = new Object[1][6];
                         lTabela[lLinha][0] = consumivel.getCodConsumivel();
                         lTabela[lLinha][1] = consumivel.getNomConsumivel();
                         lTabela[lLinha][2] = consumivel.getDesConsumivel();
                         lTabela[lLinha][3] = consumivel.getVlrConsumivel();
-                        lTabela[lLinha][4] = consumivel.getTipConsumivel();
+                        lTabela[lLinha][4] = tipo;
                         lTabela[lLinha][5] = situacao;
                         lLinha++;
                     }
@@ -110,14 +122,41 @@ public class ConsumivelController extends BaseController<Consumivel> {
             pTabela.setSelectionMode(0);
 
             TableColumn lColumn = null;
+
+            DefaultTableCellRenderer lLeft = new DefaultTableCellRenderer();
+            DefaultTableCellRenderer lCenter = new DefaultTableCellRenderer();
+            DefaultTableCellRenderer lRight = new DefaultTableCellRenderer();
+
+            lLeft.setHorizontalAlignment(SwingConstants.LEFT);
+            lCenter.setHorizontalAlignment(SwingConstants.CENTER);
+            lRight.setHorizontalAlignment(SwingConstants.RIGHT);
+
             for (int i = 0; i < pTabela.getColumnCount(); i++) {
                 lColumn = pTabela.getColumnModel().getColumn(i);
                 switch (i) {
                     case 0:
-                        lColumn.setPreferredWidth(17);
+                        lColumn.setPreferredWidth(30);
+                        lColumn.setCellRenderer(lRight);
                         break;
                     case 1:
-                        lColumn.setPreferredWidth(140);
+                        lColumn.setPreferredWidth(120);
+                        lColumn.setCellRenderer(lLeft);
+                        break;
+                    case 2:
+                        lColumn.setPreferredWidth(230);
+                        lColumn.setCellRenderer(lLeft);
+                        break;
+                    case 3:
+                        lColumn.setPreferredWidth(45);
+                        lColumn.setCellRenderer(lCenter);
+                        break;
+                    case 4:
+                        lColumn.setPreferredWidth(90);
+                        lColumn.setCellRenderer(lCenter);
+                        break;
+                    case 5:
+                        lColumn.setPreferredWidth(60);
+                        lColumn.setCellRenderer(lCenter);
                         break;
                 }
             }
