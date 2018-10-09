3b94e213748d234005b7cfb165e342a9050b42c3
junior.lenhart <junior.lenhart@LAB59-14-WIN.ROCHAS>
2018-10-08 21:39:32 -0300

    Finalização do socket

---
+++ SocketHotel/src/sockethotel/server/Server.java
@@ -0,0 +1,66 @@
+package sockethotel.server;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class Server extends Thread {
+
+    private Socket connection;
+    private static Vector clients;
+    private static int PORT = 5000;
+
+    public static void main(String args[]) {
+        try {
+            ServerSocket server = new ServerSocket(PORT);
+
+            while (true) {
+                Socket conexao = server.accept();
+                new Server(conexao).start();
+            }
+        } catch (IOException e) {
+            System.out.println("IOException: " + e);
+        }
+    }
+
+    public Server(Socket socket) {
+        connection = socket;
+    }
+
+    Server() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void run() {
+        try {
+            BufferedReader entrada = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            PrintStream saida = new PrintStream(connection.getOutputStream());
+            
+            String commando = entrada.readLine();
+            if (commando == null) {
+                return;
+            }
+            
+            clients.add(saida);
+
+            String linha = entrada.readLine();
+            while (linha != null && !(linha.trim().equals(""))) {
+                sendToAll(linha);
+
+                linha = entrada.readLine();
+            }
+
+            clients.remove(saida);
+            connection.close();
+        } catch (IOException e) {
+            System.out.println("IOException: " + e);
+        }
+    }
+
+    public void sendToAll(String pAction) throws IOException {
+        Enumeration e = clients.elements();
+        while (e.hasMoreElements()) {
+            ((PrintStream) e.nextElement()).println(pAction);
+        }
+    }
+}
